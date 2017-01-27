e86e7b3de984a6a21c5403b35f62558843f37f7e
alexburn17 <pburnham@uvm.edu>
2017-01-27 09:36:36 -0500

    in class notes

---
+++ CompBio_InClassScript.R
@@ -0,0 +1,30 @@
+# P. Alexander Burnham
+# January 26, 2017
+# Computational Biology (381)
+# R Code for In-Class Use 
+
+#' ## In Class R Script
+
+# January 26, 2017
+# Set my working directory:
+setwd("/Users/phillipburnham/ComputationalBiology/CompBio")
+
+# Clear memory of characters:
+ls()
+rm(list=ls())
+
+# New Plot
+plot(runif(10))
+
+# show some data in the console 
+
+z <- rnorm(n=3, mean=10, sd=2)
+print(z)
+
+# "purl"
+
+library(knitr)
+
+
+
+
