4d8f0bc6a587429b88689593d306d690d0672a5f
alexburn17 <pburnham@uvm.edu>
2017-03-01 14:20:22 -0500

    did other peoples work for them and added it to my homework

--- Scripts/CompBio/LectureScript_1.R
+++ Scripts/CompBio/LectureScript_1.R
@@ -281,3 +281,141 @@ MyVec <- qbinom(p=c(0.025, 0.975), size = 100, prob=0.75)
 barplot(height = MyVec)
 MyVec
 
+# Neg binomial: number of failures (values of MyVec) is a series of bernouli trials with probability of p, before we reach a target of successes.
+
+MyVec <- dnbinom(x=seq(0,40), size=5, prob=0.5) # until you get five heads (how many tails or failures)
+names(MyVec) <- seq(0,40)
+barplot(height=MyVec) # distribution of failure times
+
+# example with lower prob and les failures:
+
+MyVec <- dnbinom(x=seq(0,40), size=1, prob=0.1) # until you get five heads (how many tails or failures)
+names(MyVec) <- seq(0,40)
+barplot(height=MyVec) # distribution of failure times
+
+# alternatively sepcify mean = mu
+# and size is a dispersion parameter (small values = overdispersion)
+
+MyVec <- dnbinom(x=seq(0,40), size=1, mu=5) # until you get five heads (how many tails or failures)
+names(MyVec) <- seq(0,40)
+barplot(height=MyVec) 
+
+# change size:
+MyVec <- dnbinom(x=seq(0,40), size=5, mu=5) # until you get five heads (how many tails or failures)
+names(MyVec) <- seq(0,40)
+barplot(height=MyVec) 
+
+# pnbiom for tail distribution
+pnbinom(q=3, size=1, mu=5)
+
+# actual value for p
+qnbinom(p=0.05, size=10, mu=5)
+
+# confidence interval:
+qnbinom(p=c(0.025, 0.975), prob=0.5, size=10)
+
+# random sample from negative binomial:
+MyVec <- rnbinom(n=1000, size=1, mu=20)
+
+#calculates confidence interval of sample data:
+quantile(MyVec, prob=c(0.025, 0.975))
+
+# the theoretical values:
+qnbinom(p=c(0.025, 0.975), size=1, mu=20)
+
+##########___Continuous____Distributions___##########
+
+# Uniform Distribution (min and max value):
+limits <- seq(0,10, by=0.01)
+z <- dunif(x=limits, min=0, max=5)
+names(z) <- limits
+# represents the simple null (all are equally likely to occur)
+plot(x=limits, y=z, type="l", xlim=c(0,10))
+
+# Uniform Distribution (min and max value):
+limits <- seq(0,10, by=0.1)
+z <- punif(q=limits, min=0, max=5)
+names(z) <- limits
+plot(x=limits, y=z, type="l", xlim=c(0,10))
+
+# qunif for quantiles:
+qunif(p=c(0.025, 0.975), min=0, max=5)
+
+# runif for data:
+hist(runif(n=1000, min=0, max=5))
+
+
+# most familiar distribution -> normal distribution:
+hist(rnorm(n=100, mean=100, sd=2))
+
+# negative values become a prblem in real life:
+hist(rnorm(n=100, mean=2, sd=2))
+MyVec <- rnorm(n=100,mean=2,sd=2)
+summary(MyVec)
+TossZeroes <- MyVec[MyVec>0]
+hist(TossZeroes)
+summary(TossZeroes) # now mean doesnt center on 2
+
+# gamma distribution
+# 2 paramers (shape1 and shape2)
+# Continuous, positive values
+
+hist(rgamma(n=100, shape=1, scale=10))
+# gamma with shape = 1 gives an exponetial with scale = mean:
+hist(rgamma(n=1000, shape=0.1, scale=10))
+
+# larger shape value = more like normal distribution:
+hist(rgamma(n=1000, shape=20, scale=1))
+# mean of gamma dist = shape*scale
+# variance = (shape*scale)^2
+
+
+# Beta distribution:
+
+# continuous distribution
+# but bounded by 0 and 1
+# parmeters: shape1 = number of successes + 1
+# shape2 = number of failures + 1
+
+# shape1 = 1 and shape2 = 1 (1000 trials with no data) no coin
+hist(rbeta(shape1=1, shape2=1, n=1000), breaks=seq(0,1,length=100))
+
+# shape1 = 2 and shape2 = 1 (1 trial with a coin "1 head")
+hist(rbeta(shape1=2, shape2=1, n=1000), breaks=seq(0,1,length=100))
+
+# shape1 = 2 and shape2 = 1 (2 tosses with a coin "1 head and 1 tail")
+hist(rbeta(shape1=2, shape2=2, n=1000), breaks=seq(0,1,length=100))
+
+# 2 tosses both heads:
+hist(rbeta(shape1=3, shape2=1, n=1000), breaks=seq(0,1,length=100))
+
+# more data equal counts
+hist(rbeta(n=1000, shape1=20, shape2=20),breaks=seq(0,1,length=100))
+
+# more data not equal
+hist(rbeta(n=1000, shape1=200, shape2=100),breaks=seq(0,1,length=100))
+
+# shape parameters less thatn 1:
+hist(rbeta(n=1000, shape1=0.4, shape2=0.4),breaks=seq(0,1,length=100))
+
+# function for pulling our paramers of dist based on real data:
+
+# maximum liklihood estimation:
+# maximize p(data given a set of parameters)
+
+library(MASS)
+x <-rnorm(1000, mean=92.5, sd=2.5)
+hist(x)
+fitdistr(x, "normal")
+
+mean(x)
+sd(x)
+# try gamma
+z <- fitdistr(x, "gamma")
+str(z)
+# mean if it came from a gamma
+z$estimate[1]/z$estimate[2]
+
+# variance if it came from a gamma
+z$estimate[1]/z$estimate[2]^2
+
